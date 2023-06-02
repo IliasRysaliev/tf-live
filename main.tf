4619c0b693fdb156b5a41554ffe9ba90cd05784b
IliasRysaliev <rysaliev2011@gmail.com>
2023-06-02 08:43:00 +0600

    add

--- main.tf
+++ main.tf
@@ -5,9 +5,11 @@ provider "google" {
 resource "google_service_account" "service_account" {
   account_id   = "service-account-id"
   display_name = "Service Account"
+  project      = "extreme-world-382815"
 }
 resource "google_artifact_registry_repository" "repository" {
-  repository_id = "ilias-repo"
-  location = "us-central1"
-  format   = "DOCKER"
+  repository_id = "hello-app-docker"
+  location      = "us-central1"
+  format        = "DOCKER"
+  project       = "extreme-world-382815"
 }
