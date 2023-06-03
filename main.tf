0650dc4480591c2453960804f8cf46d45a3156ef
IliasRysaliev <rysaliev2011@gmail.com>
2023-06-03 14:14:48 +0600

    add roles

--- main.tf
+++ main.tf
@@ -7,6 +7,18 @@ resource "google_service_account" "service_account" {
   display_name = "Service Account"
   project      = "extreme-world-382815"
 }
+
+resource "google_project_iam_member" "github_token_creator" {
+  project = "extreme-world-382815"
+  role    = "roles/iam.serviceAccountTokenCreator"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
+
+resource "google_project_iam_member" "github_act_us" {
+  project = "extreme-world-382815"
+  role    = "roles/artifactregistry.writer"
+  member  = "serviceAccount:${google_service_account.service_account.email}"
+}
 resource "google_artifact_registry_repository" "repository" {
   repository_id = "hello-app-docker"
   location      = "us-central1"
